66e5dbc7dbfabaf080c4489eb0f46c9ca983076b
foxy <69487888+foxyyx@users.noreply.github.com>
2023-11-12 14:37:09 -0300

    Update animations.lua

--- animations.lua
+++ animations.lua
@@ -2,7 +2,6 @@
 animation = {}
 local private = {}
 
--->> animation:create(100, 200, 1000, 'Linear', func) | Fora do render
 function animation:create(inicial, final, time, type, func)
     assert(type(inicial) == 'number', 'INICIAL value must be a number, got '..type(inicial))
     assert(type(final) == 'number', 'FINAL value must be a number, got '..type(final))
@@ -24,13 +23,11 @@ function animation:create(inicial, final, time, type, func)
     return instance;
 end
 
--->> animation:updateTick() | Fora do render ou dentro
 function animation:updateTick()
     private[self].tick = getTickCount()
 end
 
--->> animation:updateValues(200, 100, 1000, 'Linear') | Fora do render
-function animation:updateValues(inicial, final, time, type)
+function animation:updateValues(inicial, final, time, type, func)
     assert(type(inicial) == 'number', 'INICIAL value must be a number, got '..type(inicial))
     assert(type(final) == 'number', 'FINAL value must be a number, got '..type(final))
     assert(type(time) == 'number', 'TIME value must be a number, got '..type(time))
@@ -41,20 +38,19 @@ function animation:updateValues(inicial, final, time, type)
         final = tonumber(final) or 0,
         type = type or 'Linear',
         time = tonumber(time) or 1000,
-        tick = getTickCount()
+        tick = getTickCount(),
+        __func = func
     }
 end
 
--->> animation:get() | Dentro do render para pegar o valor
 function animation:get()
     private[self].value = interpolateBetween(private[self].inicial, 0, 0, private[self].final, 0, 0, (getTickCount() - private[self].tick)private[self].time, private[self].type)
-    if (self:getValues().finalized) then
+    if (self.__func and self:getValues().finalized) then
         self.__func()
     end
     return private[self].value or 0
 end    
 
--->> animation:getValues() | Fora do render ou dentro
 function animation:getValues()
     return {
         data = private[self], 
@@ -63,12 +59,10 @@ function animation:getValues()
     }
 end
 
--->> animation:destroy() | Fora do render
 function animation:destroy()
     private[self] = nil
 end
 
--->> animation:destroyAll() | Fora do render
-function animation:destroyAll()
+function destroyAllAnimations()
     private = {}
 end
