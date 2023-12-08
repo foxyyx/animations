67d1204f6eeab9d2b8fde9a8e9d2c42196489e16
foxy <69487888+foxyyx@users.noreply.github.com>
2023-12-07 21:49:15 -0300

    Update animations.lua

--- animations.lua
+++ animations.lua
@@ -1,22 +1,21 @@
-
 animation = {}
 local private = {}
 
-function animation:create(inicial, final, time, type, func)
+function animation:create(inicial, final, time, easing, func)
     assert(type(inicial) == 'number', 'INICIAL value must be a number, got '..type(inicial))
     assert(type(final) == 'number', 'FINAL value must be a number, got '..type(final))
     assert(type(time) == 'number', 'TIME value must be a number, got '..type(time))
-    assert(type(type) == 'string', 'INICIAL value must be a number, got '..type(type))
 
     local instance = {
         inicial = inicial or 0,
         final = final or 0,
-        type = type or 'Linear',
+        easing = easing or 'Linear',
         time = time or 1000,
         tick = getTickCount(),
         value = 0,
         __atributte = {func = func, executed = false}
     }
+    private[instance] = instance
 
     setmetatable(instance, {__index = self})
 
@@ -27,22 +26,23 @@ function animation:updateTick()
     private[self].tick = getTickCount()
 end
 
-function animation:updateValues(inicial, final, time, type, func)
+function animation:updateValues(inicial, final, time, easing, func)
     assert(type(inicial) == 'number', 'INICIAL value must be a number, got '..type(inicial))
     assert(type(final) == 'number', 'FINAL value must be a number, got '..type(final))
 
     private[self] = {
         inicial = inicial or 0,
         final = final or 0,
-        type = type or private[self].type,
+        easing = easing or private[self].easing,
         time = time or private[self].time,
         tick = getTickCount(),
+        value = private[self].value or 0,
         __atributte = {func = private[self].__atributte.func, executed = false}
     }
 end
 
 function animation:get()
-    private[self].value = interpolateBetween(private[self].inicial, 0, 0, private[self].final, 0, 0, (getTickCount() - private[self].tick) / private[self].time, private[self].type)
+    private[self].value = interpolateBetween(private[self].inicial, 0, 0, private[self].final, 0, 0, (getTickCount() - private[self].tick) / private[self].time, private[self].easing)
     if (self.__atributte.func and self:getValues().finalized and not private[self].__atributte.executed) then
         self.__atributte.func()
         private[self].__atributte.executed = true
